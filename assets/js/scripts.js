f9fb33c4473e6115e4a00c8d818d6f704a6b827a
Olinto <olintolopez@hotmail.com>
2021-08-21 23:28:54 -0400

    Adding all the fles necesarys

---
+++ assets/js/scripts.js
@@ -0,0 +1,6 @@
+let menu = document.getElementById("menu");
+let menu_bar = document.querySelector(".menu-bar");
+
+menu_bar.addEventListener('click',function(){
+    menu.classList.toggle('menu-toggle');
+})
